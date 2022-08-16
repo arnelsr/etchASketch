3732cb98301bd732053869f6e16a19858bea9b44
arnelsr <arnel_villamin@yahoo.com>
2022-08-16 11:21:14 +0200

    Finish etch a sketch exercise

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//remove squares
+//removeSquares();
+
 //create main div container
 const divContainer = document.createElement('div');
 divContainer.classList.add('container');
@@ -10,48 +13,77 @@ divContainer.style.flexWrap='wrap';
 //append to body
 document.body.appendChild(divContainer)
 //create 16x16divs grid of squares
-for (let i=0;i<16;i++) {
-    //create line of grids
-    const lineDiv = document.createElement('div');
-    lineDiv.style.display = 'flex';
-    for (let j=0;j<16;j++) {
-        //create div
-        const div=document.createElement('div');
-        //flexbox display
-        div.style.display = 'flex';
-        //border solid
-        div.style.borderStyle='solid';
-        //border color black
-        div.style.borderColor='black'
-        //10x10
-        div.style.width='30px'
-        div.style.height='30px'
-        //wrap
-        div.style.justifyContent='flex-start'
-        //classbox
-        div.classList.add('smallbox');
-        //add to container
-        lineDiv.appendChild(div);
-    }
-    //append to divContainer
-    divContainer.appendChild(lineDiv);
-}
-//hover
-//select all divs with class box
-const smallBoxes=document.querySelectorAll('.smallbox');
-smallBoxes.forEach((smallBox)=> {
-    //mouseover
-    smallBox.addEventListener('mouseover', ()=>{
-        smallBox.style.backgroundColor='red';
 
-    })
-    //nomouseover
-    /*smallBox.addEventListener('mouseover', ()=>{
-        //smallBox.style.backgroundColor='red';
+drawSquares(16);
+hover();
 
-    })*/
+const button = document.querySelector('button');
+button.addEventListener('click',()=>{
+    //display popup when button is clicked
+    squares=window.prompt('Please input number of squares per line (max 100)');
+    if (squares<1) squares=1;
+    if (squares>100) squares=100;
+    //remove previous squares
+    removeSquares();
+    drawSquares(squares);
+    hover();
+    
 
 })
 
+function drawSquares(squares) {
+    for (let i=0;i<squares;i++) {
+        //create line of grids
+        const lineDiv = document.createElement('div');
+        lineDiv.classList.add('lineDiv');
+        //lineDiv.style.display = 'flex';
+        for (let j=0;j<squares;j++) {
+            //create div
+            const div=document.createElement('div');
+             //compute square size
+            let width=(32/squares)*30;
+            let widthpx=width+'px';
+            //flexbox display
+            div.style.display = 'flex';
+            //border solid
+            div.style.borderStyle='solid';
+            //border color black
+            div.style.borderColor='black'
+            //10x10
+            //div.style.width='30px'
+            //div.style.height='30px'
+            div.style.width=widthpx;
+            div.style.height=widthpx;
+            //wrap
+            div.style.justifyContent='flex-start'
+            //classbox
+            div.classList.add('smallBox');
+            //add to container
+            lineDiv.appendChild(div);
+        }
+        //append to divContainer
+        divContainer.appendChild(lineDiv);
+    }
+}
+function removeSquares() {
+    const list = Array.from(document.getElementsByClassName('lineDiv'));
+    
+    
+    list.forEach(child =>divContainer.removeChild(child));
 
+}
+   
+    
 
+//hover
+//select all divs with class box
+function hover() {
+    const smallBoxes=document.querySelectorAll('.smallBox');
+    smallBoxes.forEach((smallBox)=> {
+        //mouseover
+        smallBox.addEventListener('mouseover', ()=>{
+            smallBox.style.backgroundColor='red';
+
+        })
+})
+}
